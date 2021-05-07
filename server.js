474e5f1e7dc8acf069c9fac8d1488d19e76988fb
mlee041317 <michelleleefit@gmail.com>
2021-05-07 16:47:05 -0700

    Repo, dependencies, server.js, all that jazz has been set up, austins couch

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 4000
+
+app.get('/', (req, res) => {
+    res.send('ITS LIPSTICK TIME BITCHESSSSSSSS')
+})
+
+app.listen(PORT, () => {
+    console.log(`Our app listening at http://localhost:${PORT}`)
+});
+
