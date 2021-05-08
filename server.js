7235b2a3ae177b6648f6cd31f10f1e8c0d58f0b5
mlee041317 <michelleleefit@gmail.com>
2021-05-07 17:10:12 -0700

    kenny senseiiiiiiii doesn't like semicolons

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = 4000;
+const PORT = 4000
 
 app.get('/', (req, res) => {
-    res.send('ITS LIPSTICK TIME BITCHESSSSSSSS')
+    res.send('ITS LIPSTICK TIME BROADS')
 })
 
 app.listen(PORT, () => {
