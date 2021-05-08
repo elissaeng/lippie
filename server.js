8dd8a84ac5fd9cf3ec62aa037c8a54c5ba125e36
elissaeng@gmail.com <elissaeng@Elissas-MacBook-Pro.local>
2021-05-08 11:47:18 -0700

    started files

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 4000;
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('ITS LIPSTICK TIME BITCHESSSSSSSS')
+    res.render('home.ejs')
 })
 
 app.listen(PORT, () => {
