09927e2f7ec51988064e637a6043cf88fb5ae484
mlee041317 <michelleleefit@gmail.com>
2021-05-10 09:48:37 -0700

    added Schema and stuff idk

---
+++ models/Lipstick.js
@@ -0,0 +1,22 @@
+// Lipstick Model
+// -Img Source: Photo URL -Name: String
+// -Description: String
+// -Shades: String
+// -Ingredients: String -Application Technique: String -Price: Number
+
+const mongoose = require("mongoose");
+
+
+const lipstickSchema = new mongoose.Schema({
+    image: String,
+    description: String,
+    shades: String,
+    ingredients: String,
+    applicationTechnique: String,
+    price: Number
+})
+
+const LipstickModel = mongoose.model('Lipstick', lipstickSchema);
+
+module.exports = LipstickModel;
+
