33ce6dbfee0b6c3b00fee34523870ab9ef06bcb2
elissaeng@gmail.com <elissaeng@Elissas-MacBook-Pro.local>
2021-05-10 13:54:04 -0700

    Michelle and I own the world! haha

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const { response } = require('express');
 const express = require('express');
 const app = express();
 const PORT = 4000;
@@ -6,53 +5,61 @@ const db = require('./models/index.js');
 
 app.set('view engine', 'ejs');
 
-
-
+// Home Route
 app.get('/', (req, res) => {
-
     res.render('home.ejs');
 });
 
-// app.get('/products', (req, res) => {
-//     res.send('<h1>Lipstick Name</h1>');
-// });
-
-// index route
+// Index Route
 app.get('/lipsticks', (req, res) => {
     db.Lipstick.find({}, (err, lipsticks) => {
         if (err) {
             return console.log(err)
         } console.log(lipsticks)
         res.render('lipsticks/lipstickIndex.ejs', {
-            allLipsticks: lipsticks
+            allLipsticks: lipsticks,
         });
+    });
+});
+
+//Create Route to create/add new lipstick
+app.post('/lipsticks/lipstickNew.ejs', (req, res) => {
+    if (err) {
+        return console.log(err);
     }
-
-    )
+    db.Lipstick.create(req.body, (err, newLipstick) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(newLipstick);
+    
+        res.redirect('/lipsticks');
+    });
 });
 
 
-// //Create Route to create/add new lipstick
-// app.post('/lipsticks/lipstickNew.ejs', (req, res) => {
-//     if (err) {
-//         return console.log(err);
-//     }
-// });
-
-// db.Lipstick.create(request.body, (err, newLipstick) => {
-//     if (err) {
-//         return console.log(err);
-//     }
-//     console.log(newLipstick);
-
-//     response.redirect('/lipsticks');
-// });
-
-
-//To show a form to the user/admin to add new lipstick
-app.get('/lipsticks/lipstickNew.ejs', (req, res) => {
-    response.render('lipstickNew.ejs');
+//New Route - To show a form to the user/admin to add new lipstick
+app.get('/lipsticks/new', (req, res) => {
+    res.render('lipsticks/lipstickNew.ejs');
 });
+
+// Show Route
+app.get('/lipsticks/:id', (req, res) => {
+    db.LipstickModel.findById(req.params.id, (err, yayLipstick) => {
+        if (err) {
+            return console.log(err)
+        }
+        res.render('show', { lipstick: yayLipstick})
+    });
+}); 
+
+
+
+
+
+
+
+
 
 
 app.listen(PORT, () => {
