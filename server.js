7cb9f2e64862ff93c9176c8bbcbeb4c725a24f19
elissaeng@gmail.com <elissaeng@Elissas-MacBook-Pro.local>
2021-05-09 22:24:07 -0700

    messin around with routes

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ const PORT = 4000;
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('home.ejs')
-})
+    res.render('home.ejs');
+});
+
+// app.get('/products', (req, res) => {
+//     res.send('<h1>Lipstick Name</h1>');
+// });
+
 
 app.listen(PORT, () => {
     console.log(`Our app listening at http://localhost:${PORT}`)
